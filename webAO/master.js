96d685b3c369df94fa1fcbc66d745980d33fee39
sD <stoned@derpymail.org>
2020-03-27 18:47:53 +0100

    since it was broken anyway, remove it

--- webAO/master.js
+++ webAO/master.js
@@ -141,17 +141,6 @@ function onMessage(e) {
 		checkOnline(-1, "127.0.0.1:50001");
 		masterserver.close();
 	}
-	else if (header === "SN") {
-		const args = msg.split("#");
-		const i = args[1];
-		document.getElementById("masterlist").innerHTML +=
-			`<li id="server${i}" class="unavailable" onmouseover="setServ(${i})"><p>${args[5]}</p>`
-			+ `<a class="button" href="client.html?mode=watch&ip=${args[2]}:${args[4]}">Watch</a>`
-			+ `<a class="button" href="client.html?mode=join&ip=${args[2]}:${args[4]}">Join</a></li>`;
-		server_description[i] = args[6];
-		masterserver.send("SR#" + i + "#%");
-		checkOnline(i, `${args[2]}:${args[3]}`);
-	}
 	else if (header === "servercheok") {
 		const args = msg.split("#").slice(1);
 		document.getElementById("clientinfo").innerHTML = `Client version: ${version} expected: ${args[0]}`;
