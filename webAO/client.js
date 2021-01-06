396d900cac17e890700e0f56623278fbd90ea245
stonedDiscord <10584181+stonedDiscord@users.noreply.github.com>
2021-01-06 18:49:46 +0100

    don't overwrite the ooc in singleplayer

--- webAO/client.js
+++ webAO/client.js
@@ -737,10 +737,12 @@ class Client extends EventEmitter {
 	 * @param {Array} args packet arguments
 	 */
 	handleCT(args) {
+		if (mode !== "replay") {
 		const oocLog = document.getElementById("client_ooclog");
 		oocLog.innerHTML += `${prepChat(args[1])}: ${prepChat(args[2])}\r\n`;
 		if (oocLog.scrollTop > oocLog.scrollHeight - 600) {
 			oocLog.scrollTop = oocLog.scrollHeight;
+		}
 		}
 	}
 
