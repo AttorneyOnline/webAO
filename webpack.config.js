168675d2af2aa5350eb47042013d0033cb1211ad
stonedDiscord <Tukz@gmx.de>
2021-02-23 21:31:45 +0100

    provide a polyfill for webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,41 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        ui: './webAO/ui.js',
-        client: './webAO/client.js',
-        master: './webAO/master.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'webAO'),
-        filename: '[name].b.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  [
-                    '@babel/preset-env', {
-                      useBuiltIns: 'usage',
-                      targets: 'defaults',
-                      corejs: 3
-                    }
-                  ]
-                ]
-              }
-            }
+  entry: {
+    ui: './webAO/ui.js',
+    client: './webAO/client.js',
+    master: './webAO/master.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'webAO'),
+    filename: '[name].b.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env', {
+                  useBuiltIns: 'usage',
+                  targets: 'defaults',
+                  corejs: 3
+                }
+              ]
+            ]
           }
-        ]
-      },
+        }
+      }
+    ]
+  },
+  resolve: {
+    fallback: { "stream": require.resolve("stream-browserify") }
+  },
 
-    devtool: 'source-map',
-    mode: 'production'
+  devtool: 'source-map',
+  mode: 'production'
 };
