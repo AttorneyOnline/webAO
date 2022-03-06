b95861ced23ee942a1991fd9157164d8e97487c7
caleb.mabry.15@cnu.edu <caleb.mabry.15@cnu.edu>
2022-03-05 22:08:07 -0500

    ESLINT fix

--- webAO/evidence.js
+++ webAO/evidence.js
@@ -1,10 +1,10 @@
 export default [
-	"1.png",
-	"2.png",
-	"3.png",
-	"4.png",
-	"5.png",
-	"6.png",
-	"empty.png",
-	"lawyerbadge.png"
+  '1.png',
+  '2.png',
+  '3.png',
+  '4.png',
+  '5.png',
+  '6.png',
+  'empty.png',
+  'lawyerbadge.png',
 ];
