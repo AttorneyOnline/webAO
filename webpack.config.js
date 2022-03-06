ea0e9c20a9d5e28afcd8a8cbbbb5d29b318d7a3a
caleb.mabry.15@cnu.edu <caleb.mabry.15@cnu.edu>
2022-03-05 22:52:59 -0500

    Added ignore file

--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,43 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        ui: './webAO/ui.js',
-        client: './webAO/client.js',
-        master: './webAO/master.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'webAO'),
-        filename: '[name].b.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  [
-                    '@babel/preset-env', {
-                      useBuiltIns: 'usage',
-                      targets: [
-                        "defaults",
-                        "Safari > 3",
-                        "Opera > 8",
-                        "Android > 3"
-                      ],
-                      corejs: 3
-                    }
-                  ]
-                ]
-              }
-            }
-          }
-        ]
+  entry: {
+    ui: './webAO/ui.js',
+    client: './webAO/client.js',
+    master: './webAO/master.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'webAO'),
+    filename: '[name].b.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env', {
+                  useBuiltIns: 'usage',
+                  targets: [
+                    'defaults',
+                    'Safari > 3',
+                    'Opera > 8',
+                    'Android > 3',
+                  ],
+                  corejs: 3,
+                },
+              ],
+            ],
+          },
+        },
       },
+    ],
+  },
 
-    devtool: 'source-map',
-    mode: 'production'
+  devtool: 'source-map',
+  mode: 'production',
 };
