88715ee53ab4f58deea9c6f0dd7bbe0ac6d1b03e
caleb.mabry.15@cnu.edu <caleb.mabry.15@cnu.edu>
2022-03-05 21:40:08 -0500

    Alphabetized and added webpack config things

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,67 @@
 /* eslint-env node */
 
 const path = require('path');
+const dotenv = require('dotenv')
+const webpack = require('webpack')
+
+// this will update the process.env with environment variables in .env file
+dotenv.config();
 
 module.exports = {
-    entry: {
-        ui: './webAO/ui.js',
-        client: './webAO/client.js',
-        master: './webAO/master.js'
+  entry: {
+    ui: './webAO/ui.js',
+    client: './webAO/client.js',
+    master: './webAO/master.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'webAO'),
     },
-    output: {
-        path: path.resolve(__dirname, 'webAO'),
-        filename: '[name].b.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  [
-                    '@babel/preset-env', {
-                      useBuiltIns: 'usage',
-                      targets: [
-                        "defaults",
-                        "Safari > 3",
-                        "Opera > 8",
-                        "Android > 3"
-                      ],
-                      corejs: 3
-                    }
-                  ]
-                ]
-              }
-            }
+    compress: true,
+    port: 8080,
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env', {
+                  useBuiltIns: 'usage',
+                  targets: [
+                    "defaults",
+                    "Safari > 3",
+                    "Opera > 8",
+                    "Android > 3"
+                  ],
+                  corejs: 3
+                }
+              ]
+            ]
           }
-        ]
-      },
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'webAO'),
+    filename: '[name].b.js'
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }, 
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env)
+    })
+  ],
 
-    devtool: 'source-map',
-    mode: 'production'
 };
