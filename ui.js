f86ad0fe818aa5c997347687388e519bdea02e02
stonedDiscord <Tukz@gmx.de>
2022-03-06 19:20:32 +0000

    Deploying to gh-pages from @ AttorneyOnline/webAO@ccbca8b9cede972480dd1ef7a73db71a99be9609 🚀

--- ui.js
+++ ui.js
@@ -1,74 +1,74 @@
 /* eslint indent: ["error", 2] */
 
 // import GoldenLayout from "./golden/js/goldenlayout.js";
-import GoldenLayout from "golden-layout";
+import GoldenLayout from 'golden-layout';
 
 const config = {
   settings: {
     showPopoutIcon: false,
-    showCloseIcon: false
+    showCloseIcon: false,
   },
   dimensions: {
-    minItemHeight: 40
+    minItemHeight: 40,
   },
   content: [{
-    type: "row",
+    type: 'row',
     content: [{
-      type: "column",
+      type: 'column',
       width: 40,
       content: [{
-        type: "component",
+        type: 'component',
         isClosable: false,
-        componentName: "template",
-        title: "Game",
-        componentState: { id: "client_wrapper" }
-      }]
+        componentName: 'template',
+        title: 'Game',
+        componentState: { id: 'client_wrapper' },
+      }],
     },
     {
-      type: "column",
+      type: 'column',
       content: [{
-        type: "row",
+        type: 'row',
         height: 65,
         content: [{
-          type: "stack",
+          type: 'stack',
           content: [{
-            type: "component",
+            type: 'component',
             isClosable: false,
-            title: "Main",
-            componentName: "template",
-            componentState: { id: "mainmenu" }
+            title: 'Main',
+            componentName: 'template',
+            componentState: { id: 'mainmenu' },
           },
           {
-            type: "component",
+            type: 'component',
             isClosable: false,
-            title: "Log",
-            componentName: "template",
-            componentState: { id: "log" }
-          }]
+            title: 'Log',
+            componentName: 'template',
+            componentState: { id: 'log' },
+          }],
         },
         {
-          type: "component",
-          title: "Music",
+          type: 'component',
+          title: 'Music',
           width: 30,
-          componentName: "template",
-          componentState: { id: "music" }
-        }]
+          componentName: 'template',
+          componentState: { id: 'music' },
+        }],
       },
       {
-        type: "row",
+        type: 'row',
         content: [{
-          type: "component",
-          title: "Server chat",
-          componentName: "template",
-          componentState: { id: "ooc" }
-        }]
-      }]
-    }]
-  }]
+          type: 'component',
+          title: 'Server chat',
+          componentName: 'template',
+          componentState: { id: 'ooc' },
+        }],
+      }],
+    }],
+  }],
 };
 
 const golden = new GoldenLayout(config);
-golden.registerComponent("template", function (container, componentState) {
+golden.registerComponent('template', (container, componentState) => {
   const template = document.querySelector(`#${componentState.id}`);
   container.getElement().html(template.content);
   // TODO: support multiple locales
