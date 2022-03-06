03e95e916585fb9d1d6b23b6c8af3813df6c260c
caleb.mabry.15@cnu.edu <caleb.mabry.15@cnu.edu>
2022-03-05 22:05:02 -0500

    Add ESLINT

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "jquery": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-console": "off",
-        "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
-        "eqeqeq": ["error", "smart"],
-        "semi": ["error", "always"]
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
 };
