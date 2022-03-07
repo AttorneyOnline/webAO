ecb0b9f85d5968bfc3d687b51944a47937f698fd
caleb.mabry.15@cnu.edu <caleb.mabry.15@cnu.edu>
2022-03-06 19:00:38 -0500

    Added unit testing and code coverage

---
+++ webAO/iniParse.test.js
@@ -0,0 +1,36 @@
+import iniParse from './iniParse'
+
+const iniExample = `
+[Options]
+name = Matt
+showname = Matty
+
+[Emotions]
+number = 9
+1 = Normal#-#normal#0#1
+`
+describe('iniParse', () => {
+    test('should not lowercase value if key is showname', () => {
+        const ini = iniParse(`
+        [test]
+        showname = MATT
+        `)
+        expect(ini.test.showname).toBe('MATT')
+    })
+    test('should lowercase value if key is not showname', () => {
+        const ini = iniParse(`
+        [test]
+        party = TIME
+        `)
+        expect(ini.test.party).toBe('time')
+    })
+    test('should parse sections', () => {
+        const parsedIni = iniParse(iniExample)
+        expect(Object.keys(parsedIni).length).toBe(2);
+    });
+    test('should parse parameters', () => {
+        const parsedIni = iniParse(iniExample)
+        expect(Object.keys(parsedIni.options).length).toBe(2);
+    })
+})
+
