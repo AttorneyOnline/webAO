eaa2b97a8c6077ba7f2610eb7c7f394a62e433ec
caleb.mabry.15@cnu.edu <caleb.mabry.15@cnu.edu>
2022-03-06 19:05:34 -0500

    Lint!

--- webAO/iniParse.test.js
+++ webAO/iniParse.test.js
@@ -1,4 +1,4 @@
-import iniParse from './iniParse'
+import iniParse from './iniParse';
 
 const iniExample = `
 [Options]
@@ -8,32 +8,32 @@ showname = Matty
 [Emotions]
 number = 9
 1 = Normal#-#normal#0#1
-`
+`;
 describe('iniParse', () => {
-    test('should not lowercase value if key is showname', () => {
-        const parsedIni = iniParse(`
+  test('should not lowercase value if key is showname', () => {
+    const parsedIni = iniParse(`
         [test]
         showname = MATT
-        `)
-        expect(parsedIni.test.showname).toBe('MATT')
-    })
-    test('should lowercase value if key is not showname', () => {
-        const parsedIni = iniParse(`
+        `);
+    expect(parsedIni.test.showname).toBe('MATT');
+  });
+  test('should lowercase value if key is not showname', () => {
+    const parsedIni = iniParse(`
         [test]
         party = TIME
-        `)
-        expect(parsedIni.test.party).toBe('time')
-    })
-    test('should parse sections', () => {
-        const parsedIni = iniParse(iniExample)
-        expect(Object.keys(parsedIni).length).toBe(2);
-    });
-    test('should parse parameters', () => {
-        const parsedIni = iniParse(iniExample)
-        expect(Object.keys(parsedIni.options).length).toBe(2);
-    })
-    test('should remove empty lines', () => {
-        const parsedIni = iniParse(`
+        `);
+    expect(parsedIni.test.party).toBe('time');
+  });
+  test('should parse sections', () => {
+    const parsedIni = iniParse(iniExample);
+    expect(Object.keys(parsedIni).length).toBe(2);
+  });
+  test('should parse parameters', () => {
+    const parsedIni = iniParse(iniExample);
+    expect(Object.keys(parsedIni.options).length).toBe(2);
+  });
+  test('should remove empty lines', () => {
+    const parsedIni = iniParse(`
         [test]
         
         
@@ -41,8 +41,7 @@ describe('iniParse', () => {
         2 = 2
 
 
-        `)
-        expect(Object.keys(parsedIni.test).length).toBe(2)
-    })
-})
-
+        `);
+    expect(Object.keys(parsedIni.test).length).toBe(2);
+  });
+});
