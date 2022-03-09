7f348dd0c85cc26b041696e290e260d4bfe2a503
stonedDiscord <Tukz@gmx.de>
2022-03-09 06:37:06 +0000

    Deploying to gh-pages from @ AttorneyOnline/webAO@679bc9be8b0459d298ac1ac3f3b7e61278c184d9 🚀

---
+++ sw.js
@@ -0,0 +1,26 @@
+const cacheName = 'webAO';
+
+// Cache all the files to make a PWA
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) =>
+      // Our application only has two files here index.html and manifest.json
+      // but you can add more such as style.css as your app grows
+      cache.addAll([
+        './',
+        './index.html',
+        '../manifest.json',
+      ])),
+  );
+});
+
+// Our service worker will intercept all fetch requests
+// and check if we have cached the file
+// if so it will serve the cached file
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then((cache) => cache.match(event.request, { ignoreSearch: true }))
+      .then((response) => response || fetch(event.request)),
+  );
+});
