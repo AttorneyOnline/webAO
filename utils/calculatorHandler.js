20ace1ab5b2656f146da0f0d43a973baf72497a1
stonedDiscord <Tukz@gmx.de>
2022-03-11 18:28:13 +0000

    Deploying to gh-pages from @ AttorneyOnline/webAO@1e073175e838f7bc7662a140bcaf7d422a395046 🚀

--- utils/calculatorHandler.js
+++
@@ -1,7 +0,0 @@
-import calculateGifLength from './calculateGifLength';
-import calculateWebpLength from './calculateWebpLength';
-
-export default {
-  '.gif': calculateGifLength,
-  '.webp': calculateWebpLength,
-};
