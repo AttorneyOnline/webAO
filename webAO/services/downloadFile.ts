f7d7e96f3d36bc2bd21d58027a8610a75933eb6c
Caleb Mabry <caleb.mabry.15@cnu.edu>
2022-03-25 13:14:41 -0400

    added unit tests

--- webAO/services/downloadFile.ts
+++ webAO/services/downloadFile.ts
@@ -1,8 +1,6 @@
 const downloadFile = (content: string, filename: string) => {
-
   const a = document.createElement('a');
   const file = new Blob([content], {type: 'text'});
-  
   a.href= URL.createObjectURL(file);
   a.download = filename;
   a.click();
