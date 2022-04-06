38e999af026c2a0e29e184c658c06349753c3b10
stonedDiscord <Tukz@gmx.de>
2022-04-06 22:28:59 +0200

    lobby version string

--- webAO/master.ts
+++ webAO/master.ts
@@ -150,6 +150,6 @@ function processServerlist(thelist: { name: string, description: string, ip: str
 }
 
 function processVersion(data: string) {
-  document.getElementById('clientinfo').innerHTML = `Client version: ${version}`;
+  document.getElementById('version').innerHTML = `Client version: ${version}`;
   document.getElementById('serverinfo').innerHTML = `Master server version: ${data}`;
 }
