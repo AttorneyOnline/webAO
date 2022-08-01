319561b177d95f88c5297435371fc3f4111deddc
stonedDiscord <Tukz@gmx.de>
2022-08-01 23:01:46 +0200

    declutter the controls a little

---
+++ webAO/dom/toggleElement.js
@@ -0,0 +1,13 @@
+/**
+ * Hides and shows any html element
+ * @param {string} element_id the id of the element to toggle
+ */
+export function toggleElement(element_id) {
+    const element = document.getElementById(element_id);
+    if (element.style.display !== 'none') {
+        element.style.display = 'none';
+    } else {
+        element.style.display = 'block';
+    }
+}
+window.toggleElement = toggleElement;
