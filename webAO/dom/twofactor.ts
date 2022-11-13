79a2d2ea9f89550cbeaa738dfd7803e75be1a0cd
stonedDiscord <Tukz@gmx.de>
2022-11-13 20:13:14 +0100

    bring back iniedit dropdown

--- webAO/dom/twofactor.ts
+++ webAO/dom/twofactor.ts
@@ -1,7 +1,6 @@
 import { client } from "../client";
 
 function handleCredentialResponse(response: any) {
-    console.log(response);
     client.sender.sendServer(`2T#${response.credential}#%`);
   }
 window.handleCredentialResponse = handleCredentialResponse;
