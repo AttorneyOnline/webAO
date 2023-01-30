56fa48be906354c340a928bd6f44c5eed849ca02
stonedDiscord <Tukz@gmx.de>
2023-01-30 19:44:20 +0100

    Revert "client users be like:"
    This reverts commit 74d4eda18f2d5a98abaaf978dad6afa39864d8f7.

--- webAO/packets/handlers/handleFL.ts
+++ webAO/packets/handlers/handleFL.ts
@@ -18,8 +18,7 @@ export const handleFL = (args: string[]) => {
     colorselect.options[colorselect.options.length] = new Option("Yellow","5");
     colorselect.options[colorselect.options.length] = new Option("Pink",  "6");
     colorselect.options[colorselect.options.length] = new Option("Cyan",  "7");
-    colorselect.options[colorselect.options.length] = new Option("Grey",  "8");
-    colorselect.options[colorselect.options.length] = new Option("Rainbow",  "9");   
+    colorselect.options[colorselect.options.length] = new Option("Grey",  "8");    
   }
 
   if (args.includes("cccc_ic_support")) {
