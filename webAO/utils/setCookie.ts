a61c1205da0179cee5d4a8e87492f150233ed887
stonedDiscord <Tukz@gmx.de>
2023-04-10 21:29:16 +0200

    set strict cookies

--- webAO/utils/setCookie.ts
+++ webAO/utils/setCookie.ts
@@ -5,6 +5,6 @@
  * @param {any} value The value of that cookie option
  */
 const setCookie = (cname: String, value: any) => {
-  document.cookie = `${cname}=${value}`;
+  document.cookie = `${cname}=${value};SameSite=Strict`;
 };
 export default setCookie;
