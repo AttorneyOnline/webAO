9dd5f62f9e5ae8fe26b959ac03b8386836185241
stonedDiscord <Tukz@gmx.de>
2023-07-27 12:11:48 +0200

    category

---
+++ webAO/client/isCategory.ts
@@ -0,0 +1,6 @@
+export const isCategory = (trackname: string) => {
+    const audioEndings = ["==", "--"];
+    return (
+        audioEndings.filter((ending) => trackname.startsWith(ending)).length === 1
+    );
+}
