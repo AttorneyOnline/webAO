7661b6c7314327b6b8523976ba89755db4d281b9
David Skoland <davidskoland@gmail.com>
2023-11-29 23:14:35 +0100

    Simplify and make fileExists proper typescript

--- webAO/utils/fileExists.ts
+++ webAO/utils/fileExists.ts
@@ -1,18 +1,7 @@
-const fileExists = async (url) => new Promise((resolve) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('HEAD', url);
-    xhr.onload = function checkLoad() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                resolve(true);
-            } else {
-                resolve(false);
-            }
-        }
-    };
-    xhr.onerror = function checkError() {
-        resolve(false);
-    };
-    xhr.send(null);
-});
-export default fileExists;
+export default async function fileExists(url: string): Promise<boolean> {
+    return fetch(url, {
+        method: 'HEAD',
+    }).then((response) => {
+        return response.ok;
+    });
+}
