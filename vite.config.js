6f31b48586279274f1dd557b96739dbc9efcdcd8
David Skoland <davidskoland@gmail.com>
2024-01-28 12:24:21 +0100

    Add vite config and move index to root

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
+
+// this will update the process.env with environment variables in .env file
+dotenv.config();
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'webAO'),
+        },
+        extensions: ['.js', '.jsx', '.tsx', '.ts', '.json'],
+    },
+    server: {
+        open: true,
+        port: 8080,
+        static: {
+            directory: path.join(__dirname, 'webAO'),
+        },
+    },
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+        sourcemap: true,
+    },
+});
