218e00467a6ee04ae863fb232c0cc11145a9c1c0
stonedDiscord <Tukz@gmx.de>
2025-09-16 10:27:24 +0200

    forgot charicons

--- webAO/client/handleCharacterInfo.ts
+++ webAO/client/handleCharacterInfo.ts
@@ -6,13 +6,12 @@ import fileExists from "../utils/fileExists";
 import { AO_HOST } from "./aoHost";
 
 export const getCharIcon = async (img: HTMLImageElement, charname: string) => {
-  const charicon_extensions = [".png", ".webp"];
   img.alt = charname;
   const charIconBaseUrl = `${AO_HOST}characters/${encodeURI(
     charname.toLowerCase(),
   )}/char_icon`;
-  for (let i = 0; i < charicon_extensions.length; i++) {
-    const fileUrl = charIconBaseUrl + charicon_extensions[i];
+  for (let i = 0; i < client.charicon_extensions.length; i++) {
+    const fileUrl = charIconBaseUrl + client.charicon_extensions[i];
     const exists = await fileExists(fileUrl);
     if (exists) {
       img.alt = charname;
