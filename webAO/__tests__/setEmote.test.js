7c7d2349c59c623130c782d03534bfa2ca554bd7
stonedDiscord <Tukz@gmx.de>
2026-01-01 16:09:13 +0100

    test syntax

--- webAO/__tests__/setEmote.test.js
+++ webAO/__tests__/setEmote.test.js
@@ -3,11 +3,11 @@ import Client from "../client.ts";
 import fileExists from "../utils/fileExists.ts";
 import transparentPng from "../constants/transparentPng.js";
 
-jest.mock("../viewport/utils/createMusic");
-jest.mock("../utils/fileExists");
-jest.mock("../viewport/utils/createSfxAudio");
-jest.mock("../viewport/utils/createShoutAudio");
-jest.mock("../viewport/utils/createTestimonyAudio");
+jest.mock("../viewport/utils/createMusic", () => ({}));
+jest.mock("../utils/fileExists", () => ({}));
+jest.mock("../viewport/utils/createSfxAudio", () => ({}));
+jest.mock("../viewport/utils/createShoutAudio", () => ({}));
+jest.mock("../viewport/utils/createTestimonyAudio", () => ({}));
 describe("setEmote", () => {
   const AO_HOST = "";
 
